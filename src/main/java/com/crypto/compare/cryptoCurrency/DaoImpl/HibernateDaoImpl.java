94dce2eb00b6a097df3b57585789045f69535de5
Gaurav <gsikri@uwaterloo.ca>
2021-01-02 17:01:26 -0500

    added new file

---
+++ src/main/java/com/crypto/compare/cryptoCurrency/DaoImpl/HibernateDaoImpl.java
@@ -0,0 +1,26 @@
+package com.crypto.compare.cryptoCurrency.DaoImpl;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.context.annotation.Bean;
+
+import com.crypto.compare.cryptoCurrency.Dao.HibernateDao;
+
+public class HibernateDaoImpl implements HibernateDao {
+
+	@Bean
+	public HibernateDaoImpl getHibernateDaoObject()
+	{
+		return new HibernateDaoImpl();
+	}
+	
+	@Override
+	public Session getSession(Class c) {
+		Configuration config = new Configuration().configure().addAnnotatedClass(c);
+		SessionFactory sf = config.buildSessionFactory();
+		Session session = sf.openSession();	
+		return session;
+	}
+
+}
